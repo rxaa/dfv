832257048112c12df01b8d103b92e3b71ceb9964
rxaa <my5980@163.com>
2018-06-13 12:53:09 +0800

    log menu fix

--- src/db/SqlLog.ts
+++ src/db/SqlLog.ts
@@ -1,9 +1,9 @@
-import {dfvLog} from "../dfvLog";
+import { dfvLog } from "../dfvLog";
 export class SqlLog {
 
     static write(str: string, err?: Error | null) {
         let d = new Date();
-        let menu = dfvLog.menu + "/sql/" + d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + ".log";
+        let menu = dfvLog.menuGet() + "/sql/" + d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + ".log";
         dfvLog.write(str, err, menu);
     }
 }
